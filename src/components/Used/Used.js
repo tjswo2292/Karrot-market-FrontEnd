9006dd1ba728f7aa14e8edd09d1e811ba2575412
tjswo2292 <tjswo2292@naver.com>
2022-07-05 12:34:36 +0900

    백엔드에 인가코드 넘기기

--- src/components/Used/Used.js
+++ src/components/Used/Used.js
@@ -1,7 +1,20 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import Main from './../common/Header/Main';
 
-function Used() {
-    return(
+useEffect(async () => {
+    const url = new URL(window.location.href);
+    const authorizationCode = url.searchParams.get('code');
+    
+    const getAccessToken = authorizationCode => {
+        let tokenData = await axios
+        .post('백엔드 api 주소', {authorizationCode})
+        .then()
+        if(authorizationCode) await getAccessToken(authorizationCode);
+    }
+}, [])
+const Used = () => {
+    return (
         <div>
             <Main />
         </div>
